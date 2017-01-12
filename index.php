8eb1d7f77efa818c457f66fe59c71c0b1d32649a
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-12 08:57:37 -0700

    Updated index.php with content

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Phase 1 of Data Design Project</title>
+	</head>
+	<body>
+		<h1>
+			Persona
+		</h1>
+			<p>
+				Molly is 60-years-old with a long track record of being an artist-educator. She is mid-level tech savvy and uses technology in her everyday life although is not a "techie" person. She uses a 2011 MacbookPro laptop and an iPhone 6. She uses google docs becuase she works with other artists/professionals across the country developing projects and needs a tool to track meeting notes, to do lists, etc. She uses google docs because it is simple and integrates well with her e-mail and her teammates' emails.
+			</p>
+
+		<h1>
+			Use Case
+		</h1>
+			<p>
+				Molly has regular Director's Meetings every two weeks. The Project Administrator is in charge of developing the agenda in google docs and sharing the draft agenda prior to the meeting. Molly is responsible for reviewing the draft agenda and adding items based on their priority. She adds comments and links to the draft agenda and then reshares the new agenda with other teammates on the call.
+			</p>
+
+		<h1>
+			Interaction Flow
+		</h1>
+				<ol>
+					<li>
+						Molly logs into her e-mail.
+					</li>
+					<li>
+						Molly opens Google Drive to access the google doc under her user account.
+					</li>
+					<li>
+						Molly opens draft agenda from the recent documents list.
+					</li>
+					<li>
+						Molly adds some comments to the document.
+					</li>
+					<li>
+						Google drive automatically saves the changes.
+					</li>
+					<li>
+						Molly copies the link to the google doc and sends the link to her teammates with an e-mail stating that she has made changes to the document.
+					</li>
+				</ol>
+
+		<h1>
+			Conceptual Model
+		</h1>
+				<ul>
+					<li>
+						One user can access a google doc on their own.
+					</li>
+					<li>
+						Multiple users can access a google doc at the same time.
+					</li>
+					<li>
+						Google doc is able to automatically update changes to all users.
+					</li>
+				</ul>
+	</body>
+</html>

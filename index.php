5eeb2ee735fcd8645cbbc6faa59fca689f274845
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-12 10:19:56 -0700

    Added meta charset to index.php Created .css file

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="UTF-8">
 		<title>Phase 1 of Data Design Project</title>
+		<link href="styles.css" rel="stylesheet">
 	</head>
 	<body>
+		
 		<h1>
 			Persona
 		</h1>
@@ -21,40 +24,40 @@
 		<h1>
 			Interaction Flow
 		</h1>
-				<ol>
-					<li>
-						Molly logs into her e-mail.
-					</li>
-					<li>
-						Molly opens Google Drive to access the google doc under her user account.
-					</li>
-					<li>
-						Molly opens draft agenda from the recent documents list.
-					</li>
-					<li>
-						Molly adds some comments to the document.
-					</li>
-					<li>
-						Google drive automatically saves the changes.
-					</li>
-					<li>
-						Molly copies the link to the google doc and sends the link to her teammates with an e-mail stating that she has made changes to the document.
-					</li>
-				</ol>
+		<ol>
+			<li>
+				Molly logs into her e-mail.
+			</li>
+			<li>
+				Molly opens Google Drive to access the google doc under her user account.
+			</li>
+			<li>
+				Molly opens draft agenda from the recent documents list.
+			</li>
+			<li>
+				Molly adds some comments to the document.
+			</li>
+			<li>
+				Google drive automatically saves the changes.
+			</li>
+			<li>
+				Molly copies the link to the google doc and sends the link to her teammates with an e-mail stating that she has made changes to the document.
+			</li>
+		</ol>
 
 		<h1>
 			Conceptual Model
 		</h1>
-				<ul>
-					<li>
-						One user can access a google doc on their own.
-					</li>
-					<li>
-						Multiple users can access a google doc at the same time.
-					</li>
-					<li>
-						Google doc is able to automatically update changes to all users.
-					</li>
-				</ul>
+		<ul>
+			<li>
+				One user can access a google doc on their own.
+			</li>
+			<li>
+				Multiple users can access a google doc at the same time.
+			</li>
+			<li>
+				Google doc is able to automatically update changes to all users.
+			</li>
+		</ul>
 	</body>
 </html>
